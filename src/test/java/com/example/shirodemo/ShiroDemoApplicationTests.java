800464b94d41c4de44dee5674036bf00940bd2fc
master <master@putaoduo.com>
2021-01-10 14:23:21 +0800

    active profiles

--- src/test/java/com/example/shirodemo/ShiroDemoApplicationTests.java
+++ src/test/java/com/example/shirodemo/ShiroDemoApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.shirodemo;
 
+import com.example.shirodemo.config.ProfileUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class ShiroDemoApplicationTests {
 
     @Test
